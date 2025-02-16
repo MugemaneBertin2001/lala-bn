de81cdaee7183d21c6c817635ab5293ae0e7bde9
Bertin M <bertin.m2001@gmail.com>
2025-02-16 22:59:00 +0200

    initial commit

---
+++ src/main/java/lala/house/lala/LalaApplication.java
@@ -0,0 +1,13 @@
+package lala.house.lala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lala/house/lala/LalaApplicationTests.java
@@ -0,0 +1,13 @@
+package lala.house.lala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
